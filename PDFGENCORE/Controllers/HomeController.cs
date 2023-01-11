c6be2389a2a860eb655046e0a81baa55a6cbadc0
Uros37 <uros.s037@gmail.com>
2023-01-11 11:40:48 +0300

    first commit

---
+++ PDFGENCORE/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PDFGENCORE.Models;
+using System.Diagnostics;
+using PdfSharpCore;
+using PdfSharpCore.Pdf;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
+using PdfSharp.Fonts;
+using PdfSharpCore.Utils;
+using PdfSharp.Drawing;
+
+namespace PDFGENCORE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            var document = new PdfDocument();
+            var page = document.AddPage();
+            document.Save("d:\\helloworld.pdf");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
